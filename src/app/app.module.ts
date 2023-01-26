454e67b5fbe1d9b24faafc2592b3e39616445eaa
DM Badhon <deathmountain20@yahoo.com>
2023-01-26 18:36:51 +0600

    added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RoomsComponent } from './rooms/rooms.component';
+import { RoomsListComponent } from './rooms/rooms-list/rooms-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoomsComponent
+    RoomsComponent,
+    RoomsListComponent
   ],
   imports: [
     BrowserModule,
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -17,45 +17,53 @@ export class RoomsComponent {
     totalRooms: 20,
   };
 
-  roomList: RoomList[] = [
-    {
-      roomType: 'Business',
-      amenities: 'Air conditioner, wi-fi',
-      price: 500,
-      photos: 'https://unsplash.com/photos/Yrxr3bsPdS0',
-      checkInTime: new Date('23-Jan-2023'),
-      checkOutTime: new Date('24-Jan-2023'),
-      rating : 4.5
-    },
-    {
-      roomType: 'Deluxe',
-      amenities: 'Air conditioner, wi-fi',
-      price: 500,
-      photos: 'https://unsplash.com/photos/Yrxr3bsPdS0',
-      checkInTime: new Date('23-Jan-2023'),
-      checkOutTime: new Date('24-Jan-2023'),
-      rating:3.5
-    },
-    {
-      roomType: 'Private Suite',
-      amenities: 'Air conditioner, wi-fi',
-      price: 500,
-      photos: 'https://unsplash.com/photos/Yrxr3bsPdS0',
-      checkInTime: new Date('23-Jan-2023'),
-      checkOutTime: new Date('24-Jan-2023'),
-      rating: 2.3
-    },
-    {
-      roomType: 'Gono',
-      amenities: 'Air conditioner, wi-fi',
-      price: 500,
-      photos: 'https://unsplash.com/photos/Yrxr3bsPdS0',
-      checkInTime: new Date('23-Jan-2023'),
-      checkOutTime: new Date('24-Jan-2023'),
-      rating: 1.3
-    }
-  ];
+  roomList: RoomList[] = [];
+
+  ngOnInit(): void {
+    this.roomList = [
+      {
+        roomType: 'Business',
+        amenities: 'Air conditioner, wi-fi',
+        price: 500,
+        photos: 'https://unsplash.com/photos/Yrxr3bsPdS0',
+        checkInTime: new Date('23-Jan-2023'),
+        checkOutTime: new Date('24-Jan-2023'),
+        rating: 4.5
+      },
+      {
+        roomType: 'Deluxe',
+        amenities: 'Air conditioner, wi-fi',
+        price: 500,
+        photos: 'https://unsplash.com/photos/Yrxr3bsPdS0',
+        checkInTime: new Date('23-Jan-2023'),
+        checkOutTime: new Date('24-Jan-2023'),
+        rating: 3.5
+      },
+      {
+        roomType: 'Private Suite',
+        amenities: 'Air conditioner, wi-fi',
+        price: 500,
+        photos: 'https://unsplash.com/photos/Yrxr3bsPdS0',
+        checkInTime: new Date('23-Jan-2023'),
+        checkOutTime: new Date('24-Jan-2023'),
+        rating: 2.3
+      },
+      {
+        roomType: 'Gono',
+        amenities: 'Air conditioner, wi-fi',
+        price: 500,
+        photos: 'https://unsplash.com/photos/Yrxr3bsPdS0',
+        checkInTime: new Date('23-Jan-2023'),
+        checkOutTime: new Date('24-Jan-2023'),
+        rating: 1.3
+      }
+    ]
+  }
   toggle() {
     this.hideRooms = !this.hideRooms;
+  }
+
+  selectRoom(room:RoomList){
+    console.log(room);
   }
 }
